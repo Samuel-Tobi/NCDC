8e5738e60127a8af3b84a610691fce9935461ab0
samuel-tobi <samueltobi52@gmail.com>
2022-08-16 09:13:57 +0100

    Delete button working

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-// const patientData = document.forms["patient-form"];
-const firstName = document.querySelector("#firstName");
-const lastName = document.querySelector("#lastName");
-const email = document.querySelector("#email");
-const gender = document.querySelector("#gender");
-const visit = document.querySelector("#checkbox");
-const submit = document.querySelector("#submit");
+const patientData = document.forms["patients-form"];
+const firstName = patientData.querySelector("#firstName");
+const lastName = patientData.querySelector("#lastName");
+const email = patientData.querySelector("#email");
+const gender = patientData.querySelector("#gender");
+const visit = patientData.querySelector("#checkbox");
+const submit = patientData.querySelector("#submit");
+const update = patientData.querySelector("#update");
+
+const patientsDetails = document.querySelector("#patients-details");
 
 // Add Patient Data to Table
 const addPatientsData = (e) => {
@@ -14,14 +17,20 @@ const addPatientsData = (e) => {
   <td>${email.value}</td>
   <td>${gender.value}</td>
   <td><a href="#">No</a></td>
-  <td><button class="btn edit">Edit</button></td>
-  <td><button class="btn delete">Delete</button></td>
+  <td><button class="edit-btn">Edit</button></td>
+  <td><button class="delete-btn">Delete</button></td>
   </tr>`;
-
-  console.log("working");
 };
-
-submit.addEventListener("click", addPatientsData);
+patientData.addEventListener("submit", addPatientsData);
 
 // Edit patient data on table
 const editPatientData = () => {};
+
+//Delete patient data from table
+const deletePatientData = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("delete-btn")) {
+    e.target.parentElement.parentElement.remove();
+  }
+};
+patientsDetails.addEventListener("click", deletePatientData);
