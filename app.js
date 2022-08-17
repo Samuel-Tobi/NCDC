4a9fd2d894e1c223ca5cfd67a6860be04cb08c8a
samuel-tobi <samueltobi52@gmail.com>
2022-08-17 05:45:21 +0100

    Edit and Update functionality working

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const lastName = patientData.querySelector("#lastName");
 const email = patientData.querySelector("#email");
 const gender = patientData.querySelector("#gender");
 const visit = patientData.querySelector("#checkbox");
-const submit = patientData.querySelector("#submit");
+// const submit = patientData.querySelector("#submit");
 const update = patientData.querySelector("#update");
 
 const patientsDetails = document.querySelector("#patients-details");
@@ -16,15 +16,59 @@ const addPatientsData = (e) => {
   <td>${firstName.value} ${lastName.value}</td>
   <td>${email.value}</td>
   <td>${gender.value}</td>
-  <td><a href="#">No</a></td>
+  <td><a href='#'>${visit.checked ? "Yes" : "No"}</a></td>
   <td><button class="edit-btn">Edit</button></td>
   <td><button class="delete-btn">Delete</button></td>
   </tr>`;
+  patientData.reset();
 };
 patientData.addEventListener("submit", addPatientsData);
 
 // Edit patient data on table
-const editPatientData = () => {};
+const editPatientData = (e) => {
+  if (e.target.classList.contains("edit-btn")) {
+    // get patient data
+    patient = e.target.parentElement.parentElement;
+
+    firstName.value = patient.children[0].innerText.split(" ")[0];
+    lastName.value = patient.children[0].innerText.split(" ")[1];
+    email.value = patient.children[1].innerText;
+    gender.value = patient.children[2].innerText.split(" ");
+    patient.children[3].innerText === "Yes"
+      ? (visit.checked = true)
+      : (visit.checked = false);
+
+    // Update Button
+    document.querySelector("#submit").style.display = "none";
+    const update = patientData.querySelector("#update");
+    update.style.display = "inline-block";
+    update.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (
+        firstName.value.trim() === "" ||
+        lastName.value.trim() === "" ||
+        email.value.trim() === ""
+      ) {
+        return;
+      }
+
+      patient.innerHTML = `<tr>
+    <td>${firstName.value} ${lastName.value}</td>
+    <td>${email.value}</td>
+    <td>${gender.value}</td>
+    <td><a href='#'>${visit.checked ? "Yes" : "No"}</a></td>
+    <td><button class="edit-btn">Edit</button></td>
+    <td><button class="delete-btn">Delete</button></td>
+    </tr>`;
+
+      patientData.reset();
+      document.querySelector("#submit").style.display = "inline-block";
+      document.querySelector("#update").style.display = "none";
+    });
+  }
+};
+patientsDetails.addEventListener("click", editPatientData);
 
 //Delete patient data from table
 const deletePatientData = (e) => {
