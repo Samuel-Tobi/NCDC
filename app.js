a1ef4940dcce64f6a72095d3ab5e6f9962ff2d20
samuel-tobi <samueltobi52@gmail.com>
2022-08-27 12:59:01 +0100

    All buttons setup perfectly

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const form = document.querySelector("#patients-form");
+const patientData = document.querySelector("form");
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const email = document.querySelector("#email");
 const gender = document.querySelector("#gender");
-const visit = document.querySelector("#visit");
+
+const update = document.querySelector("#update");
+
+// setup edit buttons
+const setupEditButtons = () => {
+  const editBtns = document.querySelectorAll(".edit-btn");
+  editBtns.forEach((editBtn) => {
+    editBtn.addEventListener("click", editPatientData);
+  });
+};
+
+// setup delete buttons
+const setupDeleteButtons = () => {
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", deletePatientData);
+  });
+};
 
 // Add Patient Data to Table
-const addPatientsData = (e) => {
+function addPatientsData(e) {
   e.preventDefault();
   document.getElementById("patients-details").innerHTML += `<tr>
   <td>${firstName.value} ${lastName.value}</td>
@@ -19,55 +36,58 @@ const addPatientsData = (e) => {
 
   // Display Success alert
   const successAlert = document.querySelector(".success-alert");
-  successAlert.style.display = "block";
+  successAlert.style.display = "inline-block";
 
-  console.log("working");
+  setupEditButtons();
+  setupDeleteButtons();
+
   // Set Success alert display to none
   function clearSuccessAlert() {
     const successAlert = document.querySelector(".success-alert");
     successAlert.style.display = "none";
   }
-
   //Timeout
   setTimeout(() => {
     clearSuccessAlert();
   }, 01000);
-  form.reset();
-};
-form.addEventListener("submit", addPatientsData);
-
-const editPatient = document.querySelector("#patients-details");
-const update = document.querySelector("#update");
+  patientData.reset();
+}
+patientData.addEventListener("submit", addPatientsData);
 
 // Edit patient data on table
-const editPatientData = (data) => {
-  if (data.className === "edit-btn") {
+function editPatientData(data) {
+  if (data.target.className === "edit-btn") {
     // get patient data
-    patient = data.parentElement.parentElement;
+    patient = data.target.parentElement.parentElement;
 
     firstName.value = patient.children[0].innerText.split(" ")[0];
     lastName.value = patient.children[0].innerText.split(" ")[1];
-    email.value = patient.children[1].innerText.split(" ");
+    email.value = patient.children[1].innerText;
     gender.value = patient.children[2].innerText.split(" ");
-    visit.value =
-      patient.children[3].innerText === "Yes"
-        ? (visit.checked = true)
-        : (visit.checked = false);
+    patient.children[3].innerText === "Yes"
+      ? (visit.checked = true)
+      : (visit.checked = false);
+  }
+  console.log("edit");
 
-    // Update Button
-    document.querySelector("#submit").style.display = "none";
-    const update = document.querySelector("#update");
-    update.style.display = "inline-block";
-    update.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (
-        firstName.value.trim() === "" ||
-        lastName.value.trim() === "" ||
-        email.value.trim() === ""
-      ) {
-        return;
-      }
-      patient.innerHTML = `<tr>
+  // show update button, hide submit button
+  document.querySelector("#update").style.display = "block";
+  document.querySelector("#submit").style.display = "none";
+
+  // update button function
+  const update = document.querySelector("#update");
+  update.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+      firstName.value.trim() === "" ||
+      lastName.value.trim() === "" ||
+      email.value.trim() === "" ||
+      gender.value.trim() === "" ||
+      visit.value.trim() === ""
+    ) {
+      return;
+    }
+    patient.innerHTML = `<tr>
       <td>${firstName.value} ${lastName.value}</td>
       <td>${email.value}</td>
       <td>${gender.value}</td>
@@ -75,63 +95,51 @@ const editPatientData = (data) => {
       <td><button class="edit-btn">Edit</button></td>
       <td><button class="delete-btn">Delete</button></td>
       </tr>`;
-      form.reset();
-      document.querySelector("#submit").style.display = "inline-block";
-      document.querySelector("#update").style.display = "none";
 
-      // const delAlert = document.querySelector(".delete-alert");
-      // delAlert[0].style.display = "none";
+    patientData.reset();
+    document.querySelector("#submit").style.display = "inline-block";
+    document.querySelector("#update").style.display = "none";
 
-      // Display Update Alert
+    setupEditButtons();
+    setupDeleteButtons();
+
+    // Update alert
+    const updateAlert = document.querySelector(".update-alert");
+    updateAlert.style.display = "block";
+
+    function clearUpdateAlert() {
       const updateAlert = document.querySelector(".update-alert");
-      updateAlert.style.display = "block";
+      updateAlert.style.display = "none";
+    }
+    // set timeout
+    setTimeout(() => {
+      clearUpdateAlert();
+    }, 01000);
+    patientData.reset();
+  });
+}
+setupEditButtons();
 
-      // Set Update alert display to none
-      function recordUpdated() {
-        const updateAlert = document.querySelector(".update-alert");
-        updateAlert.style.display = "none";
-      }
-
-      // Set Timeout
-      setTimeout(() => {
-        recordUpdated();
-      }, 01000);
-    });
-  }
-};
-editPatient.addEventListener("click", editPatientData);
-
-const deletePatient = document.querySelector(".delete-btn");
-
-// for (let btn of deletePatient) {
-//   console.log(".delete-btn");
-// }
-
-// for (let btn of deletePatient) {
-//   btn.document.querySelectorAll(".delete-btn") = deletePatient;
-// }
-
-//Delete patient data from table
+//Delete button
 const deletePatientData = (data) => {
   if (data.target.className === "delete-btn") {
     data.target.parentElement.parentElement.remove();
-
-    console.log("working");
   }
 
-  // Display Delete Alert
+  // Set delete alert
   const deleteAlert = document.querySelector(".delete-alert");
   deleteAlert.style.display = "block";
 
-  // Set delete alert to none
+  //  Set delete alert to none
   function recordDeleted() {
-    document.querySelector(".delete-alert").style.display = "none";
+    const deleteAlert = document.querySelector(".delete-alert");
+    deleteAlert.style.display = "none";
   }
 
   //Set Timeout
   setTimeout(() => {
     recordDeleted();
   }, 1000);
-  // form.reset();
+  patientData.reset();
 };
-deletePatient.addEventListener("click", deletePatientData);
+setupDeleteButtons();
